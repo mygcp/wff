e1c81a84e1dbb1e86ac454d7b795fddb371a5691
mygcp <mygcp80@gmail.com>
2024-06-23 22:01:22 +0700

    add: i will reinstall new remix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -122,7 +122,8 @@ export default function Index() {
     const exec = async () => {
       const manga = await fetch('https://api.mangadex.org/manga', {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+          'Access-Control-Allow-Origin': '*'
         }
       });
 
