3a73df9fa3eac2de68dca34f2002381f502c566d
mygcp <mygcp80@gmail.com>
2024-06-23 21:56:26 +0700

    add: add UA

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -120,7 +120,11 @@ export default function Index() {
     // };
 
     const exec = async () => {
-      const manga = await fetch('https://api.mangadex.org/chapter');
+      const manga = await fetch('https://api.mangadex.org/chapter', {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+        }
+      });
 
       console.log(await manga.json())
     }
